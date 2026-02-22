97482fb903e50c047a88bb56be17b4659ab5b571
developer-hajun <juni8828@naver.com>
2026-02-22 18:01:34 +0900

    BOJ35265 - JAVA

--- src/BOJ35265.java
+++ src/BOJ35265.java
@@ -1,7 +1,10 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ35265 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tc = Integer.parseInt(br.readLine());
